5ba84f123bd3658d852ef7cbf1060112c8f60fb3
gary <gary@cashcepat.id>
2026-04-05 21:47:50 +0800

    Update SHA256 for v1.7.0

--- Casks/route-pilot.rb
+++ Casks/route-pilot.rb
@@ -1,6 +1,6 @@
 cask "route-pilot" do
   version "1.7.0"
-  sha256 "TO_BE_FILLED_AFTER_RELEASE"
+  sha256 "d232bf12bfc811f0c251d6b10cbb80a2e58b56110d059ce1900ae97fffa93360"
 
   url "https://github.com/tdragon8113/route-pilot/releases/download/v#{version}/RoutePilot.dmg"
   name "RoutePilot"

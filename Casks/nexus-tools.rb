3ff251f0ea3854d61d7f31c66af7ecdb97c5d0e7
gary <gary@cashcepat.id>
2026-04-09 16:42:12 +0800

    Add nexus-tools v1.0.0

---
+++ Casks/nexus-tools.rb
@@ -0,0 +1,23 @@
+cask "nexus-tools" do
+  version "1.0.0"
+  sha256 "18142810fc85a85ba5c3b642b2541d8203435c4f4896a5f4277a63fbc0e7bf1a"
+
+  url "https://github.com/tdragon8113/nexus-tools/releases/download/v#{version}/NexusTools.dmg"
+  name "NexusTools"
+  desc "开发者工具箱"
+  homepage "https://github.com/tdragon8113/nexus-tools"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "NexusTools.app"
+
+  zap trash: [
+    "~/Library/Application Support/NexusTools",
+    "~/Library/Caches/com.nexus.NexusTools",
+    "~/Library/HTTPStorages/com.nexus.NexusTools",
+    "~/Library/Preferences/com.nexus.NexusTools.plist",
+  ]
+end
